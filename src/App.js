6b21f6b8af8ea3f7aa6587cb96a57529882ec6b7
watunyu555 <watunyu.y@hotmail.co.th>
2021-04-16 21:20:21 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Home from "./components/webpages/Home"
 import Homepage from "./components/webpages/Homepage"
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 export default function App() {
