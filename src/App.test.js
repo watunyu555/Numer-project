31603d1e86bb32dc757956a50ab3038003e23bc1
watunyu555 <watunyu.y@hotmail.co.th>
2021-04-21 09:29:02 +0700

    add commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Spline from './components/calculator/Interpolation/Spline';
-
+import Newton from './components/calculator/Interpolation/Newtondivide';
 it('renders welcome message', () => {
   render(<Spline />);
   expect(screen.getByText('Spline')).toBeInTheDocument();
+  render(<Newton />);
+  expect(screen.getByText('Newton Divide Difference')).toBeInTheDocument();
 });
